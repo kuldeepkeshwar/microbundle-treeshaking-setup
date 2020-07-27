72aa9c0d53e1a4dc037fd966b21b56fbc00ed386
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-07-27 12:08:26 +0530

    add bundle report link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Columns, Stack } from "layout-ui";
+import { styled } from "@filbert-js/core";
 
+const Container = styled("div")`
+  > * + * {
+    margin-top: 4rem;
+  }
+`;
 export default function App() {
   return (
-    <>
+    <Container>
       <Stack gap="2rem" align="center">
         <div>Item 1</div>
         <div>Item 2</div>
@@ -12,6 +18,9 @@ export default function App() {
         <Columns.Column>Item 1</Columns.Column>
         <Columns.Column>Item 2</Columns.Column>
       </Columns>
-    </>
+      <div>
+        <a href="/bundle-report.html">Check bundle report</a>
+      </div>
+    </Container>
   );
 }
