aedbafb6b897e755726118318762a5a6ea1f47f5
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-07-27 11:52:04 +0530

    use Columns only

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Columns, Stack } from "layout-ui";
+import { Columns } from "layout-ui";
 
 export default function App() {
   return (
-    <>
-      <Stack gap="2rem" align="center">
-        <div>Item 1</div>
-        <div>Item 2</div>
-      </Stack>
-      <Columns gap="2rem" align="center">
-        <Columns.Column>Item 1</Columns.Column>
-        <Columns.Column>Item 2</Columns.Column>
-      </Columns>
-    </>
+    <Columns gap="2rem" align="center">
+      <Columns.Column>Item 1</Columns.Column>
+      <Columns.Column>Item 2</Columns.Column>
+    </Columns>
   );
 }
